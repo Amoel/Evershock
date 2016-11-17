362ffa66bd7d60757bef195313ec43f45cc6b82d
Amoel <giller.max@googlemail.com>
2016-11-17 23:57:30 +0100

    Added MultiPathColliderComponent

---
+++ EvershockGame/EntityComponent/Components/Collider/MultiPathColliderComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityComponent.Components
+{
+    public class MultiPathColliderComponent : ColliderComponent
+    {
+        public MultiPathColliderComponent(Guid entity) : base(entity) { }
+
+        //---------------------------------------------------------------------------
+
+        public void AddPath(Vector2 start, Vector2 end)
+        {
+
+        }
+    }
+}
