f7ab9af8d8388475c7315a4b8fa7e752845d2a3c
Lu <lucky-pieper@web.de>
2017-02-18 15:04:44 +0100

    CoinAnimation, when picking something up. Its a fuckin' mess. But it works :D

--- EvershockGame/EvershockGame/Code/Components/UIComponents/CoinCollectionComponent.cs
+++ EvershockGame/EvershockGame/Code/Components/UIComponents/CoinCollectionComponent.cs
@@ -18,7 +18,11 @@ namespace EvershockGame.Code.Components
         bool m_Interpolating;
         int m_CurrentCoins;
         int m_CoinTextureCompartments;  //thats how many sprites are in the CoinAnimationSheet
+        int m_CoinWidthInCoinTexture;
+        int m_CoinTextureSpriteNumber;
+        int m_CoinAnimationCounter;
         float m_PastCoins;
+        float m_FrameDeltaTime;
         float m_CombinedDeltaTime;
         float m_InterpolationTime;
         float m_CoinTextureScale;
@@ -30,9 +34,14 @@ namespace EvershockGame.Code.Components
             m_CoinTexture = AssetManager.Get().Find<Texture2D>(ESpriteAssets.CoinAnimation);
             m_Font = AssetManager.Get().Find<SpriteFont>(EFontAssets.DebugFont);
             m_PlayerCoins = new Dictionary<Player, int>();
+
             m_InterpolationTime = 0.5f;
+            m_CoinAnimationCounter = 1;
+            m_CoinTextureSpriteNumber = 0;
             m_CoinTextureCompartments = 6;
             m_CoinTextureScale = 0.25f;
+
+            m_CoinWidthInCoinTexture = (m_CoinTexture.Width / m_CoinTextureCompartments);
         }
 
         //---------------------------------------------------------------------------
@@ -40,20 +49,27 @@ namespace EvershockGame.Code.Components
         int InterpolateDisplay(int value, float deltaTime, float durationInSeconds)
         {
             m_CombinedDeltaTime += deltaTime;
+            m_FrameDeltaTime += deltaTime;
 
             if (m_CombinedDeltaTime >= durationInSeconds)
             {
                 m_Interpolating = false;
                 return m_CurrentCoins;
             }
-                
             else
             {
-                m_Interpolating = true;
-                float temp = m_CurrentCoins - m_PastCoins;
-                return (int)(m_PastCoins + (temp * m_CombinedDeltaTime/durationInSeconds));
+                if (m_FrameDeltaTime > durationInSeconds / m_CoinTextureCompartments * m_CoinAnimationCounter)
+                {
+                    m_CoinTextureSpriteNumber++;
+                    m_FrameDeltaTime -= deltaTime;
+
+                    if (m_CoinTextureSpriteNumber >= m_CoinTextureCompartments + 1)
+                        m_CoinTextureSpriteNumber = 1;
+                }
+
+                return (int)(m_PastCoins + ((m_CurrentCoins - m_PastCoins) * m_CombinedDeltaTime / durationInSeconds));
             }
-        }
+        }   
 
         //---------------------------------------------------------------------------
 
@@ -73,7 +89,11 @@ namespace EvershockGame.Code.Components
                     if (m_Interpolating)
                         m_CombinedDeltaTime -= m_InterpolationTime;
                     else
+                    {
+                        m_Interpolating     = true;
                         m_CombinedDeltaTime = 0;
+                        m_FrameDeltaTime    = 0;
+                    }
                 });
             }
         }
@@ -86,10 +106,22 @@ namespace EvershockGame.Code.Components
                 Rectangle bounds = transform.Bounds();
                 batch.DrawString(m_Font, InterpolateDisplay(m_CurrentCoins, deltaTime, m_InterpolationTime).ToString(), new Vector2(bounds.Center.X, bounds.Y), Color.White);
 
-                int width_offset = (int)(m_CoinTexture.Width / m_CoinTextureCompartments * m_CoinTextureScale);
-                int magic_pixel_offset = 10;
-                batch.Draw(texture:m_CoinTexture, sourceRectangle:new Rectangle(0,0, m_CoinTexture.Width / m_CoinTextureCompartments, m_CoinTexture.Height), destinationRectangle:new Rectangle(bounds.Center.X - width_offset, bounds.Y - (int)(m_CoinTexture.Height / 2 * m_CoinTextureScale) + magic_pixel_offset, (int)(m_CoinTexture.Width / m_CoinTextureCompartments * m_CoinTextureScale), (int)(m_CoinTexture.Height * m_CoinTextureScale)), color:Color.White);
-                
+                if (m_Interpolating)
+                {
+                    batch.Draw(
+                        texture: m_CoinTexture,
+                        sourceRectangle: new Rectangle(m_CoinWidthInCoinTexture * m_CoinTextureSpriteNumber - 1, 0, m_CoinWidthInCoinTexture, m_CoinTexture.Height),
+                        destinationRectangle: new Rectangle(bounds.Center.X - (int)(m_CoinWidthInCoinTexture * m_CoinTextureScale), bounds.Y - (int)(m_CoinTexture.Height / 2 * m_CoinTextureScale) + 10, (int)(m_CoinWidthInCoinTexture * m_CoinTextureScale), (int)(m_CoinTexture.Height * m_CoinTextureScale)),
+                        color: Color.White);
+                }
+                else
+                {
+                    batch.Draw(
+                        texture: m_CoinTexture,
+                        sourceRectangle: new Rectangle(0, 0, m_CoinWidthInCoinTexture, m_CoinTexture.Height),
+                        destinationRectangle: new Rectangle(bounds.Center.X - (int)(m_CoinWidthInCoinTexture * m_CoinTextureScale), bounds.Y - (int)(m_CoinTexture.Height / 2 * m_CoinTextureScale) + 10, (int)(m_CoinWidthInCoinTexture * m_CoinTextureScale), (int)(m_CoinTexture.Height * m_CoinTextureScale)),
+                        color: Color.White);
+                }
             }
             else
                 AssertManager.Get().Show("UITransformComponent in CoinCollectionComponent is null");
